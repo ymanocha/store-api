c9e7cebb0c67b8de2e11429d17135c9dd73fc3b8
ymanocha <yashmanocha19@gmail.com>
2025-09-10 02:11:51 +0530

    sort functionality

---
+++ controllers/products.js
@@ -0,0 +1,38 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic =  async (req,res) =>{
+
+    const products =  await Product.find({}).sort('-name price')
+    res.status(200).json({products, nbHits: products.length })
+}
+
+const getAllProducts = async (req,res) => {
+    const {featured, company, name, sort} = req.query
+    const queryObject = {}
+    //HOW DOES IF EVEN WORk
+    if (featured){
+       queryObject.featured = featured === 'true'? true : false
+    }
+    if (company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = {$regex: name, $options: "i"} //i is for case sensitive or lower case
+    }
+    // console.log(queryObject)
+    // const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
+
+    res.status(200).json({products, nbHits: products.length})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic}
